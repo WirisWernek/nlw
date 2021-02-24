4621763dff796f2c99b3d0011fa56edac54fbe7f
Wiris Wernek <rafaeljwerneck43@gmail.com>
2021-02-23 22:40:13 -0300

    corrigindo o bug do banco de dados

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 import { UserController } from "./controllers/UserController";
-
-const userController = new UserController();
 
 const router = Router();
 
-router.post("./users", userController.create);
+const usersController = new UserController();
+
+router.post("/users", usersController.create);
 
 export { router };
